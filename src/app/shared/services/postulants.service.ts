c343854378737f88faf6ab129cd96a3283cfcd44
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-07-17 19:09:41 -0400

    Add validation for scanner

--- src/app/shared/services/postulants.service.ts
+++ src/app/shared/services/postulants.service.ts
@@ -37,57 +37,83 @@ export class PostulantsService extends DataService<Postulant> {
       .valueChanges();
   }
 
-  acceptPostulant(postulant: Postulant): void {
+  acceptPostulant(postulant: Postulant): boolean {
     postulant.accepted = !postulant.accepted;
     this.upsertData(postulant);
+    return true;
   }
 
-  markCredentialAsSent(postulant: Postulant): void {
+  markCredentialAsSent(postulant: Postulant): boolean {
     if (postulant.accepted) {
       postulant.credentialSent = !postulant.credentialSent;
       this.upsertData(postulant);
+      return true;
     }
+
+    return false;
   }
 
-  confirmPostulantAssistance(postulant: Postulant): void {
+  confirmPostulantAssistance(postulant: Postulant): boolean {
     if (postulant.accepted && postulant.credentialSent) {
       postulant.confirmedAssistant = !postulant.confirmedAssistant;
       this.upsertData(postulant);
+      return true;
     }
+
+    return false;
   }
 
-  checkInAssistant(postulant: Postulant): void {
+  checkInAssistant(postulant: Postulant): boolean {
     if (postulant.accepted) {
       postulant.checkIn = !postulant.checkIn;
       this.upsertData(postulant);
+      return true;
     }
+
+    return false;
   }
 
-  markFeeForLunchAsReceived(postulant: Postulant): void {
+  markFeeForLunchAsReceived(postulant: Postulant): boolean {
     if (postulant.accepted && postulant.checkIn) {
       postulant.feeForLunchReceived = !postulant.feeForLunchReceived;
       this.upsertData(postulant);
+      return true;
     }
+
+    return false;
   }
 
-  markLunchAsDelivered(postulant: Postulant): void {
-    if (postulant.accepted && postulant.checkIn) {
+  markLunchAsDelivered(postulant: Postulant): boolean {
+    if (
+      postulant.accepted &&
+      postulant.checkIn &&
+      postulant.feeForLunchReceived
+    ) {
       postulant.lunchDelivered = !postulant.lunchDelivered;
       this.upsertData(postulant);
+      return true;
     }
+
+    return false;
   }
 
-  markFirstSnackAsDelivered(postulant: Postulant): void {
+  markFirstSnackAsDelivered(postulant: Postulant): boolean {
     if (postulant.accepted && postulant.checkIn) {
       postulant.firstSnackDelivered = !postulant.firstSnackDelivered;
       this.upsertData(postulant);
+      return true;
     }
+
+    return false;
   }
 
-  markSecondSnackAsDelivered(postulant: Postulant): void {
+  markSecondSnackAsDelivered(postulant: Postulant): boolean {
     if (postulant.accepted && postulant.checkIn) {
       postulant.secondSnackDelivered = !postulant.secondSnackDelivered;
       this.upsertData(postulant);
+      return true;
     }
+
+    return false;
   }
 }
