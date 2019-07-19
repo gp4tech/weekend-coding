c523c15c51ff1d9bb0e17b33eba32da936571ecf
Mauricio Arce <mauricio.arce.mat@gmail.com>
2019-07-19 04:10:54 -0400

    Convert the app into a pwa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+
+import { ModalDirective } from './shared/directives/modal/modal.directive';
 
 @Component({
   selector: 'wc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  @ViewChild('updateModal', { static: true })
+  updateModal: ModalDirective;
+
+  constructor(private updateService: SwUpdate) {}
+
+  ngOnInit(): void {
+    this.updateService.available.subscribe(event => {
+      console.log('current version is', event.current);
+      console.log('available version is', event.available);
+      this.updateModal.modalInstance.open();
+    });
+    this.updateService.activated.subscribe(event => {
+      console.log('old version was', event.previous);
+      console.log('new version is', event.current);
+    });
+  }
+
+  updateApp(): void {
+    window.location.reload();
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { LayoutModule } from './layout/layout.module';
+import { ModalModule } from './shared/directives/modal/modal.module';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CoreModule, AppRoutingModule, LayoutModule],
+  imports: [
+    CoreModule,
+    AppRoutingModule,
+    LayoutModule,
+    ModalModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    })
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
